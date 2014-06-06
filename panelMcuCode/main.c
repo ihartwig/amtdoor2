089b3d2f153595c180fe1467b0f19e0e8285c4d9
Ian Hartwig <ihartwig@andrew.cmu.edu>
2014-06-05 22:53:28 -0700

    code to parse and report tag

--- panelMcuCode/main.c
+++ panelMcuCode/main.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
 
+
+
+
 /** LUFA CDC Class driver interface configuration and state information. This structure is
  *  passed to all CDC Class driver functions, so that multiple instances of the same class
  *  within a device can be differentiated from one another.
@@ -34,17 +37,22 @@ USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface =
 
 // standard file streams for the serial ios
 static FILE USBSerialStream;
-static FILE HWSerialStream;
-char USBSerialBuffer[128];
-char HWSerialBuffer[128];
+// static FILE HWSerialStream;
+
+/* Global Data */
+char_buffer_t command_buffer;
+char_buffer_t tag_buffer;
 
 
 int main (void) {
+  initBuffer(&command_buffer);
+  initBuffer(&tag_buffer);
+
   setupHardware();
 
   /* Create a regular character stream for the interface so that it can be used with the stdio.h functions */
   CDC_Device_CreateStream(&VirtualSerial_CDC_Interface, &USBSerialStream);
-  Serial_CreateStream(&HWSerialStream);
+  // Serial_CreateStream(&HWSerialStream);
 
   GlobalInterruptEnable();
 
@@ -57,34 +65,69 @@ int main (void) {
     // PORTE &= ~_BV(6); //set pin 0 on port B low
     // _delay_ms(500);
 
-    /* Must throw away unused bytes from the host, or it will lock up while waiting for the device */
-    // CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
-
-    // echo string
-    uint16_t numBytes = CDC_Device_BytesReceived(&VirtualSerial_CDC_Interface);
-    if(numBytes > 0) {
-      fgets(&USBSerialBuffer, 128, &USBSerialStream);
-      fputs(&USBSerialBuffer, &USBSerialStream);
+    // echo command string
+    while(CDC_Device_BytesReceived(&VirtualSerial_CDC_Interface) > 0) {
+      if(readOrDumpBuffer(
+           (char)CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface),
+           &command_buffer)) {
+        fputs(&command_buffer, &USBSerialStream);
+      }
     }
 
-    // check for new data from RFID card reader
-    bool newBytes = Serial_IsCharReceived();
-    if(newBytes) {
-      fgets(&HWSerialBuffer, 128, &HWSerialStream);
-      fputs(&HWSerialBuffer, &USBSerialStream);
-      fputs("\r\n", &USBSerialStream);
+    // check for new data from RFID card reader, return when ready
+    while(Serial_IsCharReceived()) {
+      if(readOrDumpBuffer(
+           (char)Serial_ReceiveByte(),
+           &tag_buffer)) {
+        if(strlen(tag_buffer.buffer) == TAG_LENGTH) {
+          fputs("TAG|", &USBSerialStream);
+          fputs(&tag_buffer, &USBSerialStream);
+          fputs("\r\n", &USBSerialStream);
+        } else {
+          fputs("BADREAD\r\n", &USBSerialStream);
+        }
+      }
     }
-
-    /* Echo all received data on the second CDC interface */
-    // int16_t ReceivedByte = CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
-    // if (!(ReceivedByte < 0))
-    //   CDC_Device_SendByte(&VirtualSerial_CDC_Interface, (uint8_t)ReceivedByte);
 
     // call maintenance functions
     CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
     USB_USBTask();
   }
   return 0;
+}
+
+
+void initBuffer(char_buffer_t *buffer) {
+  buffer->buffer[0] = '\0';
+  buffer->pos = 0;
+  buffer->size = CHAR_BUFFER_SIZE;
+}
+
+
+/**
+  Serial buffer control function.
+  Attempt to read available characters into the buffer until either the buffer
+  is full or a new line is reached. Omits newline, but adds null terminator.
+  Return: true if complete line available.
+**/
+bool readOrDumpBuffer(char new_char, char_buffer_t *buffer) {
+  // fprintf(&USBSerialStream, "char:%c,pos:%d\r\n", new_char, buffer->pos);
+  if(new_char == '\r') {
+    // ignore
+    return false;
+  } else if(new_char == '\n') {
+    buffer->buffer[buffer->pos] = '\0';
+    buffer->pos = 0;
+    return true;
+  } else {
+    if(buffer->pos + 1 >= buffer->size) {
+      // if no space for null terminator, this buffer is full.
+      buffer->pos = 0;
+    }
+    buffer->buffer[buffer->pos] = new_char;
+    buffer->pos++;
+    return false;
+  }
 }
 
 
@@ -97,7 +140,7 @@ void setupHardware(void) {
   clock_prescale_set(clock_div_1);
 
   // start hw serial
-  Serial_Init(2400, false);
+  Serial_Init(RFID_UART_BAUD, false);
 
   // start virtual serial
   USB_Init();

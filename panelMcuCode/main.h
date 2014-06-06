f0fb45c6a3458a67405c11afdfce460d5945bc85
Ian Hartwig <ihartwig@andrew.cmu.edu>
2014-06-05 22:53:28 -0700

    code to parse and report tag

--- panelMcuCode/main.h
+++ panelMcuCode/main.h
@@ -1,44 +1,49 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-  /* Includes: */
-  #include <avr/io.h>
-  #include <avr/wdt.h>
-  #include <avr/power.h>
-  #include <avr/interrupt.h>
-  #include <string.h>
-  #include <stdio.h>
 
-  #include "descriptors.h"
+/* Includes: */
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+#include <string.h>
+#include <stdio.h>
 
-  #include <LUFA/Drivers/Peripheral/Serial.h>
-  #include <LUFA/Drivers/USB/USB.h>
-  #include <LUFA/Platform/Platform.h>
+#include "descriptors.h"
 
-  /* Macros: */
-  #define LEDDDR  DDRE
-  #define LEDPORT PORTE
-  #define LEDPIN  PINE
-  #define LEDPINN 6
+#include <LUFA/Drivers/Peripheral/Serial.h>
+#include <LUFA/Drivers/USB/USB.h>
+#include <LUFA/Platform/Platform.h>
 
-  /** LED mask for the library LED driver, to indicate that the USB interface is not ready. */
-  // #define LEDMASK_USB_NOTREADY      LEDS_LED1
 
-  /** LED mask for the library LED driver, to indicate that the USB interface is enumerating. */
-  // #define LEDMASK_USB_ENUMERATING  (LEDS_LED2 | LEDS_LED3)
+/* Macros */
+#define LEDDDR  DDRE
+#define LEDPORT PORTE
+#define LEDPIN  PINE
+#define LEDPINN 6
 
-  /** LED mask for the library LED driver, to indicate that the USB interface is ready. */
-  // #define LEDMASK_USB_READY        (LEDS_LED2 | LEDS_LED4)
+#define CHAR_BUFFER_SIZE 128
+#define TAG_LENGTH 10
+#define RFID_UART_BAUD 2400
 
-  /** LED mask for the library LED driver, to indicate that an error has occurred in the USB interface. */
-  // #define LEDMASK_USB_ERROR        (LEDS_LED1 | LEDS_LED3)
 
-  /* Function Prototypes: */
-  void setupHardware(void);
+/* Data Structures */
+typedef struct char_buffer_t {
+  char buffer[CHAR_BUFFER_SIZE];
+  size_t pos;
+  size_t size;
+} char_buffer_t;
 
-  void EVENT_USB_Device_Connect(void);
-  void EVENT_USB_Device_Disconnect(void);
-  void EVENT_USB_Device_ConfigurationChanged(void);
-  void EVENT_USB_Device_ControlRequest(void);
+
+/* Function Prototypes: */
+bool readOrDumpBuffer(char new_char, char_buffer_t *buffer);
+void setupHardware(void);
+
+void EVENT_USB_Device_Connect(void);
+void EVENT_USB_Device_Disconnect(void);
+void EVENT_USB_Device_ConfigurationChanged(void);
+void EVENT_USB_Device_ControlRequest(void);
+
 
 #endif

248ac959ee9527cfaa9a51e9ea5190ef04e52e47
Ian Hartwig <ihartwig@andrew.cmu.edu>
2014-06-07 23:21:40 -0700

    implemented 1ms system timer

--- panelMcuCode/main.h
+++ panelMcuCode/main.h
@@ -23,6 +23,11 @@
 #define LEDPIN  PINE
 #define LEDPINN 6
 
+#define RFIDEN_DDR  DDRD
+#define RFIDEN_PORT PORTD
+#define RFIDEN_PIN  PIND
+#define RFIDEN_PINN 7
+
 #define CHAR_BUFFER_SIZE 128
 #define TAG_LENGTH 10
 #define RFID_UART_BAUD 2400
@@ -36,9 +41,17 @@ typedef struct char_buffer_t {
 } char_buffer_t;
 
 
+typedef enum device_state_t {
+  IDLE,
+  VERIFYING,
+  OPEN
+} device_state_t;
+
+
 /* Function Prototypes: */
 bool readOrDumpBuffer(char new_char, char_buffer_t *buffer);
 void setupHardware(void);
+void initSystemTime(void);
 
 void EVENT_USB_Device_Connect(void);
 void EVENT_USB_Device_Disconnect(void);
